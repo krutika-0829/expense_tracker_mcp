c68c0c455afc7b90118b61ec3b6262c235ca0c2d
unknown <krutikavairagade424@gmail.com>
2026-09-11 17:14:32 +0530

    use Railway port

--- main.py
+++ main.py
@@ -129,4 +129,7 @@ def delete_expense(user_id, expense_id):
 
 
 if __name__ == "__main__":
-    mcp.run(transport="http", host="0.0.0.0", port=8000)
+    
+
+    port = int(os.environ.get("PORT", 8000))  
+    mcp.run(transport="http", host="0.0.0.0", port=port)
