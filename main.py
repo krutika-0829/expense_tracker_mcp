1f875e8689b4b8a3848e387574df8eef5cba8ff2
unknown <krutikavairagade424@gmail.com>
2026-09-11 17:22:37 +0530

    modified main.py

--- main.py
+++ main.py
@@ -130,6 +130,4 @@ def delete_expense(user_id, expense_id):
 
 if __name__ == "__main__":
     
-
-    port = int(os.environ.get("PORT", 8000))  
-    mcp.run(transport="http", host="0.0.0.0", port=port)
+    mcp.run(transport="http", host="0.0.0.0", port=8000)
